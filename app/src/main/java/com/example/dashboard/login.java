3a1762ae5bfd57779086ce4ea82bd195206e6bbb
Maverick-2123 <rishabhiam21@gmail.com>
2023-04-03 16:23:00 +0530

    Login Register Done Firebase pending

--- app/src/main/java/com/example/dashboard/login.java
+++ app/src/main/java/com/example/dashboard/login.java
@@ -2,13 +2,37 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    Button button;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button)findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+        button2 = (Button)findViewById(R.id.button4);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),signup.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/dashboard/signup.java
+++ app/src/main/java/com/example/dashboard/signup.java
@@ -7,27 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class loginRegister extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     Button button;
-    Button sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_register);
-        button = (Button)findViewById(R.id.button);
+        setContentView(R.layout.activity_signup);
+
+        button = (Button)findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),login.class));
-            }
-        });
-        sign_up = (Button)findViewById(R.id.button2);
-        sign_up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),signup.class));
             }
         });
     }
